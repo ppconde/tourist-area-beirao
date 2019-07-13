e61a75973f7fcef5c4f590a7cc329d2545d5b1be
Pedro Conde <pepconde.1993@gmail.com>
2019-07-13 18:15:30 +0100

    append data to html test

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,13 @@ console.log("Entrou no javascript");
 
 let response;
 
-$.getJSON("http://localhost:3000/posts/1", function(data) {
+$.getJSON("https://json-db-beirao.herokuapp.com/posts", function(data) {
   //data is the JSON string
   response = data;
-  console.log(response);
+  console.log(response[0]);
+  $("header").append(
+    `<p>Id: ${response[0].id}, title: ${response[0].title}, content: ${
+      response[0].content
+    }</p>`
+  );
 });
