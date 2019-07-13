770de822b9d999dd3ad99209b4698b59b9b2b031
Pedro Conde <pepconde.1993@gmail.com>
2019-07-13 17:41:02 +0100

    added db fetching

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,10 @@
 //Insert your javascript code here
 console.log("Entrou no javascript");
+
+let response;
+
+$.getJSON("http://localhost:3000/posts/1", function(data) {
+  //data is the JSON string
+  response = data;
+  console.log(response);
+});
