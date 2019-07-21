1d3a56ff24496581dbf12958f9ad06ab6b124266
Pedro Conde <pepconde.1993@gmail.com>
2019-07-21 01:47:07 +0100

    First attempt of  host recomendations

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,2 @@
 //Insert your javascript code here
 console.log("Entrou no javascript");
-
-let response;
-
-$.getJSON("https://json-db-beirao.herokuapp.com/posts", function(data) {
-  //data is the JSON string
-  response = data;
-  console.log(response[0]);
-  $("header").append(
-    `<p>Id: ${response[0].id}, title: ${response[0].title}, content: ${
-      response[0].content
-    }</p>`
-  );
-});
